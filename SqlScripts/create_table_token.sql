3fd467e52eb5e998748a46edbb388437509c74eb
Thiago Vieira Reginaldo <thiagopesquisa42@gmail.com>
2018-04-22 21:05:52 -0300

    Script de criação do banco de dados. Modelo Entidade-Relacionamento das Tabelas do banco de dados.

--- SqlScripts/create_table_token.sql
+++
@@ -1,8 +0,0 @@
-USE plagiarism_detection;
-
-CREATE TABLE IF NOT EXISTS `token` (
-  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-  `occurence` int UNSIGNED NOT NULL,
-  `text` varchar(1000) NOT NULL,
-  PRIMARY KEY (`id`)
-) DEFAULT CHARSET=utf8;
